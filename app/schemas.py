5a3ca91087277e9b2f7c6a7a2e2158ded6d42603
Abhinavmamidwar <abhinavmamidwar22@gmail.com>
2026-03-01 23:52:47 +0530

    Added Swagger request examples

--- app/schemas.py
+++ app/schemas.py
@@ -1,27 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 
 class Transaction(BaseModel):
-    amount: float
-    oldbalanceOrg: float
-    newbalanceOrig: float
-    oldbalanceDest: float
-    newbalanceDest: float
-    type: str
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "amount": 2500,
-                "oldbalanceOrg": 5000,
-                "newbalanceOrig": 2500,
-                "oldbalanceDest": 10000,
-                "newbalanceDest": 12500,
-                "type": "TRANSFER"
-            }
-        }
-
+    amount: float = Field(..., example=2500)
+    oldbalanceOrg: float = Field(..., example=5000)
+    newbalanceOrig: float = Field(..., example=2500)
+    oldbalanceDest: float = Field(..., example=10000)
+    newbalanceDest: float = Field(..., example=12500)
+    type: str = Field(..., example="TRANSFER")
 
 
 class PredictionResponse(BaseModel):
